433245218d173d6bb750f795aa4478edc4450bbb
pikachumetal <pikachu.metal@gmail.com>
2016-07-25 12:53:45 +0200

    app

---
+++ BBPP01/IoCApp/Biz/Foo.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using IoCApp.Biz.Logger;
+
+namespace IoCApp.Biz
+{
+    class Foo
+    {
+        //private readonly IContainer _cntr;
+        private readonly ILogger _logger;
+
+        ///// <summary>
+        ///// Amb aixó no sabem de que depenem, es una implementacio service locator(factoria) - Antipatró
+        ///// </summary>
+        ///// <param name="container"></param>
+        //public Foo(IContainer container)
+        //{
+        //    _cntr = container;
+        //}
+
+        /// <summary>
+        /// Aquí si que veiem que depenem d'unlogger
+        /// </summary>
+        /// <param name="logger"></param>
+        public Foo(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int Bar()
+        {
+            //Antipatró
+            //_logger = _cntr.Resolve<ILogger>();
+            _logger.Log("Returning the answer for all...");
+            return 42;
+        }
+    }
+}
---
+++ BBPP01/IoCApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using IoCApp.Biz;
+using IoCApp.Biz.Logger;
+using System;
+
+namespace IoCApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Config Autofac
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleLogger>().As<ILogger>();
+            builder.RegisterType<Foo>();
+
+            var container = builder.Build();
+            
+            #endregion
+
+            var logger = container.Resolve<ILogger>();
+            logger.Log("Hello World!!!");
+
+            //Correcte
+            var foo = container.Resolve<Foo>();
+            logger.Log(string.Format("The anwser for all is : {0}",foo.Bar()));
+            
+            ////Incorrecte
+            //var foo2 = new Foo(container);
+            //logger.Log(string.Format("The anwser for all is : {0}", foo2.Bar()));
+
+            Console.ReadKey();
+        }
+    }
+}
